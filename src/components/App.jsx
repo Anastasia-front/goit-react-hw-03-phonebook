5dc9e22dae21142cfaa82ded93d39958973a9e7c
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-04-14 19:44:51 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,19 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addContact = text => {
     const isDuplicate = this.state.contacts.some(
       contact => contact.text.name.toLowerCase() === text.name.toLowerCase()
